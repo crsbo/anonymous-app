df6d46477a1f30d2d073e44d192b16f4faef6f18
crsbo <202401983@pua.edu.eg>
2026-01-30 01:42:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -226,9 +226,10 @@ def utility_processor():
 with app.app_context():
     # سطر التحديث (امسحه بعد أول رن)
     # db.drop_all() 
-    db.drop_all()
+    
     db.create_all()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
