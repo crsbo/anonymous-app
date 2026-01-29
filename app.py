695595fd27c8b08c13f485d864378f9e441983ed
crsbo <202401983@pua.edu.eg>
2026-01-29 16:44:59 +0200

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -139,8 +139,7 @@ def logout():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # ده السطر اللي بيبني الجداول الجديدة أول ما ترفع الكود
+        db.create_all()
     
-    # ده التعديل الذهبي لريلواي
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
